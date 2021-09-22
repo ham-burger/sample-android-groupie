2ced0be7758c464a4b5ff9abdc8e549a840be9d4
kotaro.kudo <devkudou@gmail.com>
2021-09-22 12:07:51 +0900

    add databinding and viewbinding

--- app/src/main/java/com/hamburger/sampleandroidgroupie/MainActivity.kt
+++ app/src/main/java/com/hamburger/sampleandroidgroupie/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.hamburger.sampleandroidgroupie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hamburger.sampleandroidgroupie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.text.text = "hello!"
     }
 }
