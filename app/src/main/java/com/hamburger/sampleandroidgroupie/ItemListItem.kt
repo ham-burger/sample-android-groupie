59596153d2a51320d6ef17e7ee2ea6983870f4e6
kotaro.kudo <devkudou@gmail.com>
2021-09-23 11:31:17 +0900

    add click action

--- app/src/main/java/com/hamburger/sampleandroidgroupie/ItemListItem.kt
+++ app/src/main/java/com/hamburger/sampleandroidgroupie/ItemListItem.kt
@@ -1,18 +1,27 @@
 package com.hamburger.sampleandroidgroupie
 
+import android.view.View
 import com.hamburger.sampleandroidgroupie.databinding.ListItemItemBinding
 import com.xwray.groupie.databinding.BindableItem
 
-class ItemListItem(private val item: Item) : BindableItem<ListItemItemBinding>() {
+class ItemListItem(private val item: Item, private val itemListItemAction: ItemListItemAction) :
+    BindableItem<ListItemItemBinding>() {
     override fun bind(viewBinding: ListItemItemBinding, position: Int) {
         viewBinding.also {
             it.name.text = item.name
             it.description.text = item.description
             it.price.text = item.price.toString()
+            viewBinding.root.setOnClickListener {
+                itemListItemAction.onClick(it, item)
+            }
         }
     }
 
     override fun getLayout(): Int {
         return R.layout.list_item_item
     }
+}
+
+interface ItemListItemAction {
+    fun onClick(view: View, item: Item)
 }
--- app/src/main/java/com/hamburger/sampleandroidgroupie/MainActivity.kt
+++ app/src/main/java/com/hamburger/sampleandroidgroupie/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.hamburger.sampleandroidgroupie
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.hamburger.sampleandroidgroupie.databinding.ActivityMainBinding
 import com.xwray.groupie.GroupieAdapter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ItemListItemAction {
     private lateinit var binding: ActivityMainBinding
     private val items by lazy {
         (1..100).map {
@@ -23,10 +25,17 @@ class MainActivity : AppCompatActivity() {
         binding.text.text = "hello!"
         val adapter = GroupieAdapter()
         val listItems = items.map {
-            ItemListItem(it)
+            ItemListItem(it, this)
         }
         adapter.addAll(listItems)
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
+
+    override fun onClick(view: View, item: Item) {
+        Snackbar.make(view, "メッセージ ${item.name}", Snackbar.LENGTH_SHORT).show();
+    }
+
+
 }
